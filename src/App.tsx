69133e06f3579def865c512a15f8db72e71a1884
lendog75 <lendog75@gmail.com>
2024-04-12 10:57:47 -0600

    tweaks to overall app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Box } from "@mui/material";
 import "./App.css";
-import { Header } from "./components/header/Header";
 import { Hero } from "./components/hero/Hero";
 import { Home } from "./pages";
 import Image2 from "src/assets/images/2.jpg";
 export const App = () => {
   return (
-    <Box sx={{ width: "100%", border: "1px solid yellow" }}>
-      {/* <Header /> */}
-
+    <Box>
       <Hero />
       <Home />
       <img src={Image2} style={{ width: "100%" }} alt="test" />
